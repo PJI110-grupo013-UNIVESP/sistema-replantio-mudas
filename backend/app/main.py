87ce79f419d489fa924471243154c1893432fed8
Felipe Caetano <fecae95@gmail.com>
2026-03-07 21:03:35 -0300

    feat: Frontend-to-Backend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API Replantio de Mudas")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=("*"),  # Aqui vai a URL quando estiver em produção
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def read_root():
-        return {"message": "API for the Seedling Replanting System is now operational!"}
+    return {"message":
+            "API for the Seedling Replanting System is now operational!"
+            }
+
 
 @app.get("/status")
 def status():
-    return {"status": "OK", "Database": "Pending"}
+    return {"status": "Online",
+            "Database": "Conectado",
+            "Mudas_Cadastradas": 3000
+            }
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,15 +1,67 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const statusApi = ref('Carregando...')
+const totalMudas = ref(0)
+const erroConexao = ref(false)
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:8000/status')
+    const data = await response.json()
+
+    statusApi.value = data.status
+    totalMudas.value = data.Mudas_Cadastradas
+  } catch (erro) {
+    console.error("Error to load datas:", erro)
+    statusApi.value = "Offline"
+    erroConexao.value = true
+  }
+})
+</script>
+
 <template>
   <div class="dashboard-container">
+
     <div class="card">
       <h3>Visão Geral do Replantio</h3>
       <p>Este é o Dashboard principal</p>
     </div>
 
     <div class="card">
-      <h3>Indicadores Físicos</h3>
-      <p>Aqui ficarão os dados da Curva S...</p>
+      <h3>Status do Sistema</h3>
+      <p>
+        <strong>Servidor: </strong>
+        <span :class="{ 'text-error': erroConexao, 'text-ok': !erroConexao }">
+          {{ statusApi }}
+        </span>
+      </p>
+
+      <p v-if="!erroConexao">
+        <strong>Mudas em Estoque: </strong> {{ totalMudas }}
+      </p>
+
+      <p v-if="erroConexao" class="text-error">
+        Não foi possível conectar ao Backend. Verifique se a API está rodando!
+      </p>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.dashboard-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.text-ok {
+  color: #2d6a4f;
+  font-weight: bold;
+}
+
+.text-error {
+  color: #e63946;
+  font-weight: bold;
+}
+</style>
